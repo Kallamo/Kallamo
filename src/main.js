d6420ca0eed92b5f4a960b505f2b0f3d2e56f828
Jonathan Ferreira da Conceição <jonathanfcon@gmail.com>
2026-06-15 13:30:54 -0300

    fix: disable spellcheck in webPreferences

--- src/main.js
+++ src/main.js
@@ -75,7 +75,8 @@ function createWindow () {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
-      devTools: !app.isPackaged
+      devTools: !app.isPackaged,
+      spellcheck: false
     }
   });
 
